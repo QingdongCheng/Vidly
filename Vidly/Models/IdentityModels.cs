1e54ff53a96d6aabf3b8bfdfb6c541deaad2caa8
Qingdong Cheng <32751656+QingdongCheng@users.noreply.github.com>
2018-03-23 05:02:12 +0800

    Migration
    First commit after enable-migrations

---
+++ Vidly/Models/IdentityModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class IdentityModels
+    {
+    }
+
+    public class CustomerContext: DbContext
+    {
+        public DbSet<Customer> CustomerItem { get;  set; }
+        public DbSet<Movie> MovieItem { get; set; }
+
+    }
+}
